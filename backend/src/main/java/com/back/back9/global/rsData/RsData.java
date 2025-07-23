d28c600d921827ad279f4dd33eb1b4fc8c067ca7
GarakChoi <tpghks312@naver.com>
2025-07-24 03:34:25 +0900

    add login
    # Conflicts:
    #	backend/src/main/java/com/back/back9/global/rq/Rq.java
    
    # Conflicts:
    #	backend/src/main/java/com/back/back9/global/jpa/entity/BaseEntity.java

--- backend/src/main/java/com/back/back9/global/rsData/RsData.java
+++ backend/src/main/java/com/back/back9/global/rsData/RsData.java
@@ -1,40 +1,4 @@
 package com.back.back9.global.rsData;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record RsData<T>(
-        @JsonProperty("status")
-        String status,
-        @JsonProperty("code")
-        int code,
-        @JsonProperty("message")
-        String message,
-        @JsonProperty("result")
-        T result,
-        @JsonIgnore
-        String resultCode,
-        @JsonIgnore
-        int statusCode,
-        @JsonIgnore
-        String msg,
-        @JsonIgnore
-        T data
-) {
-    public RsData(String resultCode, String msg) {
-        this(resultCode, msg, null);
-    }
-
-    public RsData(String resultCode, String msg, T data) {
-        this(
-                resultCode.startsWith("2") ? "success" : "fail",
-                Integer.parseInt(resultCode.split("-", 2)[0]),
-                msg,
-                data,
-                resultCode,
-                Integer.parseInt(resultCode.split("-", 2)[0]),
-                msg,
-                data
-        );
-    }
+public class RsData {
 }
