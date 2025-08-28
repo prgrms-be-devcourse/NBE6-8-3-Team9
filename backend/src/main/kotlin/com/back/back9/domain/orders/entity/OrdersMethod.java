9710aa50a7ac213dab8ecdf7d6dc542bba96b850
GarakChoi <tpghks312@naver.com>
2025-08-28 15:15:56 +0900

    work

--- backend/src/main/kotlin/com/back/back9/domain/orders/entity/OrdersMethod.java
+++
@@ -1,6 +0,0 @@
-package com.back.back9.domain.orders.entity;
-
-public enum OrdersMethod {
-    LIMIT, // 지정가 주문
-    MARKET // 시장가 주문
-}
--- backend/src/main/kotlin/com/back/back9/domain/tradeLog/entity/TradeType.java
+++
@@ -1,7 +0,0 @@
-package com.back.back9.domain.tradeLog.entity;
-
-public enum TradeType {
-    BUY,
-    SELL,
-    CHARGE; // 충전
-}
